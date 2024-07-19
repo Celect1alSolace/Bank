81eb7b3545a52ecf001848fa27ed9bee6362d7e1
Celest1alSolace <anvarjonov.m777@gmail.com>
2024-07-19 20:20:43 +0500

    All Done

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let elModalWrapper = document.querySelector(".modal-wrapper")
+let elModal = document.querySelector(".modal")
+
+function handleMenuIconClick(){
+    elModalWrapper.classList.toggle("show")
+}
