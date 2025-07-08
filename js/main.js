3a1f3ca10c538c63451a467bf3f58a4ef3ddd733
Celest1alSolace <anvarjonov.m777@gmail.com>
2025-07-08 17:00:50 +0500

    Test_Spin_Of_Luck_1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-let elModalWrapper = document.querySelector(".modal-wrapper")
-let elClose = document.querySelector(".close-icon")
-let elHamburger = document.querySelector(".hamburger-icon")
+let wheel = document.querySelector('.wheel');
+let spinBtn = document.querySelector('.spinBtn');
+let value = Math.ceil(Math.random() * 3600);
 
-function handleMenuIconOpen(){
-    elModalWrapper.classList.add("show")
-    elClose.classList.add("close-show")
-    elHamburger.classList.add("hide")
+const clickSound = new Audio('./sounds/Spinning_sound.mp3');
+const winSound = new Audio('./sounds/Win_Sound.mp3');
+
+function fadeOutVolume(audioElement) {
+  const interval = 1500; // каждые 500 мс
+  const fadeStep = 0.1;
+
+  const fade = setInterval(() => {
+    if (audioElement.volume > fadeStep) {
+      audioElement.volume -= fadeStep;
+    } else {
+      audioElement.volume = 0;
+      clearInterval(fade);
+    }
+  }, interval);
 }
-function handleMenuIconClose(){
-    elModalWrapper.classList.remove("show")
-    elClose.classList.remove("close-show")
-    elHamburger.classList.remove("hide")
-}
+
+spinBtn.onclick = function () {
+    clickSound.currentTime = 0;
+    clickSound.volume = 1.0;
+    clickSound.play();
+
+    // Начинаем уменьшать громкость после запуска звука
+    fadeOutVolume(clickSound); // 🟡 ВОТ ТУТ добавляем
+
+    wheel.style.transition = 'transform 5s ease-out'; // не забудь!
+    wheel.style.transform = "rotate(" + value + "deg)";
+    value += Math.ceil(Math.random() * 3600);
+
+    setTimeout(() => {
+        winSound.play();
+    }, 5000);
+};
